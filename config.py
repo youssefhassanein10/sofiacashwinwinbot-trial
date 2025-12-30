adb5748947584f2b5cffaa19d4fd8b4d8e662dc5
youssefhassanein10 <youssefhassanein075@gmail.com>
2025-12-30 13:10:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+# Токен бота (в Render добавьте как переменную окружения BOT_TOKEN)
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7479880371:AAHemgaC1OO2Ni-8ClbH9aYG4c8_FXoIQik')
 
-ADMINS = [7940060404]
-SUPPORT_ADMIN_ID = 7940060404
+# ID администратора (ваш Telegram ID)
+ADMIN_ID = int(os.getenv('ADMIN_ID', '7940060404'))
+
+# Настройки базы данных
+DB_NAME = "database.db"
+
+# Минимальные суммы
+MIN_DEPOSIT = 100  # минимальное пополнение
+MIN_WITHDRAW = 200  # минимальный вывод
